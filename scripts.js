683453bf6cff0b375488a59c25228af222b9ff9e
Rebecca Johnson <rebeccajohnson@commure.com>
2022-11-30 16:08:33 -0500

    .

--- scripts.js
+++ scripts.js
@@ -12,7 +12,16 @@ function ProjectPageLoad()
     loadGitHubTable();
    
 }
+function NestPageLoad()
+{
+    getCurrentTemperature();
+    writeCurrentTemp();
+    writeFreezing();
+}
 
+//global variables
+var G_tempInC;
+var G_TimeTempUpdated;
 
 function loadGitHubTable()
 {
@@ -82,6 +91,72 @@ function loadGitHubTable()
     xhttp.open("GET","https://api.github.com/users/rafjohnson/repos",true)
     xhttp.setRequestHeader("accept","application/vnd.github+json");
     xhttp.send();
+}
+
+function getCurrentTemperature()
+{
+    //44.69794771753228, -73.12395138237159
+
+    
+    //build URL:
+    var URL = "https://api.openweathermap.org/data/2.5/weather?lat=44.69&lon=-73.12&appid=5cce785d90f7af4ce1bb4a32c3dda15a&units=metric"
+
+    var http = new XMLHttpRequest();
+    http.open("GET", URL, false);
+    
+    
+    http.setRequestHeader("accept","application/vnd.github+json");
+    http.send();
+    
+    var response = http.responseText;
+    //parse json
+    var JObj = JSON.parse(response);
+
+    var tempInC = JObj["main"]["temp"];
+
+    G_tempInC= tempInC;
+    G_TimeTempUpdated = UNIXtoUTC(JObj["dt"])
+
+
+
+
+}
+function UNIXtoUTC(UNIXTime)
+{
+    //https://stackoverflow.com/a/847196/7858537
+    // Create a new JavaScript Date object based on the timestamp
+    // multiplied by 1000 so that the argument is in milliseconds, not seconds.
+    var date = new Date(UNIXTime * 1000);
+    // Hours part from the timestamp
+    var hours = date.getHours();
+    // Minutes part from the timestamp
+    var minutes = "0" + date.getMinutes();
+    // Seconds part from the timestamp
+    var seconds = "0" + date.getSeconds();
+
+    // Will display time in 10:30:23 format
+    var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+    
+    return formattedTime;
+}
+
+function writeCurrentTemp()
+{
+    document.getElementById("currTemp").innerText=G_tempInC;
+    document.getElementById("UpdatedTime").innerText=G_TimeTempUpdated;
+}
+function writeFreezing()
+{
+    var tempInC = G_tempInC;
+    if(tempInC>0)
+    {
+        document.getElementById("freezing").innerText = "Above Freezing";
+        document.getElementById("freezing").style.color="red";
+    }
+    else{
+        document.getElementById("freezing").innerText = "Below Freezing";
+        document.getElementById("freezing").style.color="blue";
+    }
 }
 
 function getLastModified()
